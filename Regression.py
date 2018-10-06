8247fcaa7c1c2e783865e90670a7721b3cb61cac
VarthanV <varthanv26018@gmail.com>
2018-10-06 19:41:29 +0530

    New commit

---
+++ Regression.py
@@ -0,0 +1,43 @@
+''' @ Author Vishnu Varthan, Oct 6,2018'''
+#Required Dependencies
+import pandas as pd
+import math
+import quandl
+import numpy as np
+
+
+''' Data Preprocessing '''
+df = quandl.get("WIKI/GOOGL")
+
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close','Adj. Volume']]
+
+
+df['HL_PCT'] = (df['Adj. High']-df['Adj. Low'])/df['Adj. Close']*100.0
+
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) /df['Adj. Open']*100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+''' In this model we are predicting the percentage increase based on the Features'''
+df.dropna(inplace=True)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+x=np.array(df.drop(['label'],1))
+y=np.array(df['label']).transpose
+''' Cleaning data removing headers and the other stuffs and using it suitable for computation
+ '''
+x=preprocessing.scale(x)
+x_train,y_train,x_test,y_test=train_test_split(x,y)
+clf=LinearRegression()
+clf.fit(x_train,y_train)
+
