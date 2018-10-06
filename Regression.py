f43f1d486c58e4003790b0cc10894e5f84b1c5ee
VarthanV <varthanv26018@gmail.com>
2018-10-06 20:13:28 +0530

    Issue Resolved

--- Regression.py
+++ Regression.py
@@ -18,10 +18,13 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) /df['Adj. Open']*100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
+
 forecast_col = 'Adj. Close'
-df.fillna(value=-99999, inplace=True)
+df.fillna(-999,inplace=True)
+
+
 forecast_out = int(math.ceil(0.01 * len(df)))
-df['label'] = df[forecast_col].shift(-forecast_out)
+label = df[forecast_col].shift(-forecast_out)
 
 
 ''' In this model we are predicting the percentage increase based on the Features'''
@@ -32,12 +35,14 @@ from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-x=np.array(df.drop(['label'],1))
-y=np.array(df['label']).transpose
+x=np.array(df.drop([label],1,inplace=True))
+y=np.array(df[label])
 ''' Cleaning data removing headers and the other stuffs and using it suitable for computation
  '''
 x=preprocessing.scale(x)
+x=x[:-forecast_out]
 x_train,y_train,x_test,y_test=train_test_split(x,y)
 clf=LinearRegression()
 clf.fit(x_train,y_train)
-
+confidence=clf.score(x_train,y_train)
+print("The accuracy of model is {}".format(confidence))
